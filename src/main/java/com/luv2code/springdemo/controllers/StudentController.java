6bf3c0c122357d23333d909fe3cde8a000952b24
Heroglaber <alexander.budanin@gmail.com>
2020-11-04 16:40:40 +0300

    Change folder structure for Chad mvc app

--- src/main/java/com/luv2code/springdemo/controllers/StudentController.java
+++ src/main/java/com/luv2code/springdemo/controllers/StudentController.java
@@ -1,15 +1,18 @@
-package com.luv2code.springdemo.mvc;
+package com.luv2code.springdemo.controllers;
 
+import com.luv2code.springdemo.models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-	@RequestMapping("/showForm")
+	@GetMapping("/showForm")
 	public String showForm(Model theModel) {
 		
 		// create a student object
@@ -21,7 +24,7 @@ public class StudentController {
 		return "student-form";
 	}
 	
-	@RequestMapping("/processForm")
+	@PostMapping("/processForm")
 	public String processForm(@ModelAttribute("student") Student theStudent) {
 		
 		// log the input data
