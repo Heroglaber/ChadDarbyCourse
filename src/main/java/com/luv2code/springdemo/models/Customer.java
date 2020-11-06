9e0fba314ddc60843aaa088fa12784f52cc7d0fc
Heroglaber <alexander.budanin@gmail.com>
2020-11-06 07:51:37 +0300

    169 unit: Added custom validation rule @CourseCode with ConstraintValidator (CourseCodeConstraintValidator)

--- src/main/java/com/luv2code/springdemo/models/Customer.java
+++ src/main/java/com/luv2code/springdemo/models/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.models;
 
+import com.luv2code.springdemo.validation.CourseCode;
+
 import javax.validation.constraints.*;
 
 public class Customer {
@@ -17,6 +19,12 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
 
+    @CourseCode(value = "JAV")
+    private String courseCode;
+
+
+
+
     public String getPostalCode() {
         return postalCode;
     }
@@ -29,9 +37,7 @@ public class Customer {
         return freePasses;
     }
 
-    public void setFreePasses(Integer freePasses) {
-        this.freePasses = freePasses;
-    }
+    public void setFreePasses(Integer freePasses) { this.freePasses = freePasses; }
 
     public String getFirstName() {
         return firstName;
@@ -48,4 +54,8 @@ public class Customer {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getCourseCode() { return courseCode; }
+
+    public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
 }
