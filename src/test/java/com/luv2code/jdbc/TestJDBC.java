a0aa7e98962317121365d0739d3881945e0dc34c
Буданин Александр Алексеевич <abudanin@hse.ru>
2020-11-18 13:05:07 +0300

    28 chapter: build a database web app init

--- src/test/java/com/luv2code/jdbc/TestJDBC.java
+++ src/test/java/com/luv2code/jdbc/TestJDBC.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class TestJDBC {
     public static void main(String[] args) {
 
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-        String user = "hbstudent";
-        String pass = "hbstudent";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        String user = "springstudent";
+        String pass = "springstudent";
 
         try {
             System.out.println("Connecting to database: " + jdbcUrl);
